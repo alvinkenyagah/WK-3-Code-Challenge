51ec729652ff9102a7ffced1cdd651e6701c2ba2
alvinkenyagah <alvin47k@gmail.com>
2023-04-01 15:53:30 +0300

    Add background image and changed li text color

--- script.js
+++ script.js
@@ -64,82 +64,83 @@
 //     .catch((err) => console.log(err));
 // }
 
-
-
 function movie() {
-    fetch(`http://localhost:3000/films`)
-      .then((res) => res.json())
-      .then((data) => {
-        const movieData = data;
-        const filmsList = document.querySelector("#films");
+  fetch(`http://localhost:3000/films`)
+    .then((res) => res.json())
+    .then((data) => {
+      const movieData = data;
+      const filmsList = document.querySelector("#films");
 
-        for (let i = 0; i < movieData.length; i++) {
-          const li = document.createElement("li");
-          const a = document.createElement("a");
-          a.href = "#";
-          a.textContent = movieData[i].title;
-          li.appendChild(a);
-          filmsList.appendChild(li);
+      for (let i = 0; i < movieData.length; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.style.color = "white";
+        a.style.textDecoration = "none";
 
-          a.addEventListener("click", () => {
-            const display = document.querySelector("#display");
-            display.innerHTML = "";
+        a.textContent = movieData[i].title;
+        li.appendChild(a);
+        filmsList.appendChild(li);
 
-            const title = document.createElement("h1");
-            const poster = document.createElement("img");
-            const description = document.createElement("p");
-            const showTime = document.createElement("h4");
-            const runtime = document.createElement("h4");
-            const tickets = document.createElement("h4");
-            const buy = document.createElement("button");
+        a.addEventListener("click", () => {
+          const display = document.querySelector("#display");
+          display.innerHTML = "";
 
-            buy.classList.add("btn", "btn-success");
+          const title = document.createElement("h1");
+          const poster = document.createElement("img");
+          const description = document.createElement("p");
+          const showTime = document.createElement("h4");
+          const runtime = document.createElement("h4");
+          const tickets = document.createElement("h4");
+          const buy = document.createElement("button");
 
-            title.textContent = movieData[i].title;
-            poster.src = movieData[i].poster;
-            description.textContent = `Description: ${movieData[i].description}`;
-            showTime.textContent = `Movie starts at: ${movieData[i].showtime}`;
-            runtime.textContent = `Duration: ${movieData[i].runtime} minutes`;
-            tickets.textContent = `Tickets remaining: ${
-              movieData[i].capacity - movieData[i].tickets_sold
-            }`;
-            buy.textContent = `BUY TICKET`;
-            display.appendChild(title);
-            display.appendChild(poster);
-            display.appendChild(description);
-            display.appendChild(showTime);
-            display.appendChild(runtime);
-            display.appendChild(buy);
-            display.appendChild(tickets);
+          buy.classList.add("btn", "btn-success");
 
-            buy.addEventListener("click", (e) => {
-              e.preventDefault();
-              movieData[i].tickets_sold++;
-              const remainingTickets =
-                movieData[i].capacity - movieData[i].tickets_sold;
+          title.textContent = movieData[i].title;
+          poster.src = movieData[i].poster;
+          description.textContent = `Description: ${movieData[i].description}`;
+          showTime.textContent = `Movie starts at: ${movieData[i].showtime}`;
+          runtime.textContent = `Duration: ${movieData[i].runtime} minutes`;
+          tickets.textContent = `Tickets remaining: ${
+            movieData[i].capacity - movieData[i].tickets_sold
+          }`;
+          buy.textContent = `BUY TICKET`;
+          display.appendChild(title);
+          display.appendChild(poster);
+          display.appendChild(description);
+          display.appendChild(showTime);
+          display.appendChild(runtime);
+          display.appendChild(buy);
+          display.appendChild(tickets);
 
-              fetch(`http://localhost:3000/films/${movieData[i].id}`, {
-                method: "PATCH",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  tickets_sold: movieData[i].tickets_sold,
-                }),
+          buy.addEventListener("click", (e) => {
+            e.preventDefault();
+            movieData[i].tickets_sold++;
+            const remainingTickets =
+              movieData[i].capacity - movieData[i].tickets_sold;
+
+            fetch(`http://localhost:3000/films/${movieData[i].id}`, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                tickets_sold: movieData[i].tickets_sold,
+              }),
+            })
+              .then((response) => {
+                if (response.ok) {
+                  tickets.textContent = `Tickets remaining: ${remainingTickets}`;
+                }
               })
-                .then((response) => {
-                  if (response.ok) {
-                    tickets.textContent = `Tickets remaining: ${remainingTickets}`;
-                  }
-                })
-                .catch((err) => console.log(err));
+              .catch((err) => console.log(err));
 
-              tickets.textContent = `Tickets remaining: ${remainingTickets}`;
-            });
+            tickets.textContent = `Tickets remaining: ${remainingTickets}`;
           });
-        }
-      })
-      .catch((err) => console.log(err));
-  }
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+}
 
-  movie();
+movie();
